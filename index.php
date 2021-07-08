16cf97cce074c5014515d145f0cdca8ae59ea4e7
krisdianwidya <krisdianwidya@gmail.com>
2021-07-08 14:09:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quisquam architecto, non explicabo, vitae ex nesciunt, hic odit molestiae asperiores ullam nobis dignissimos quia adipisci animi libero voluptatem itaque repudiandae.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
